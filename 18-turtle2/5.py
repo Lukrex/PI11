3d6fe35646535299799e79b427c7872d83b2eb22
majchrakl23 <luksatv14@gmail.com>
2024-03-06 11:12:40 +0100

    change 18-turtle2

--- 18-turtle2/5.py
+++ 18-turtle2/5.py
@@ -10,15 +10,15 @@ for i in range(pocet):
     t.pd()
     turtles.append(t)
 
-# for i in range(4):
-#     for t in turtles:
-#         t.fd(50)
-#         t.rt(90)
-
-for t in turtles:
-    for i in range(4):
+for i in range(4):
+    for t in turtles:
         t.fd(50)
         t.rt(90)
+
+# for t in turtles:
+#     for i in range(4):
+#         t.fd(50)
+#         t.rt(90)
 
 
 turtle.mainloop()
